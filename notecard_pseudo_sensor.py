ec949adeb7b3ac7e49c59badb834e6ae4925a024
TJ VanToll <tj.vantoll@gmail.com>
2021-10-22 15:03:11 -0400

    feat: Adding support for circuitpython

--- notecard_pseudo_sensor.py
+++ notecard_pseudo_sensor.py
@@ -1,11 +1,36 @@
+import board
+import busio
 import notecard
-from periphery import I2C
 import random
+import sys
+
+use_circuitpython = sys.implementation.name == 'circuitpython'
+
+def NotecardExceptionInfo(exception):
+    name = exception.__class__.__name__
+    return sys.platform + ": " + name \
+        + ": " + ' '.join(map(str, exception.args))
 
 class NotecardPseudoSensor:
   def __init__(self):
-    port = I2C("/dev/i2c-1")
-    self.card = notecard.OpenI2C(port, 0, 0)
+
+    if use_circuitpython:
+        try:
+            port = busio.I2C(board.SCL, board.SDA)
+        except Exception as exception:
+            raise Exception("error opening port: "
+                            + NotecardExceptionInfo(exception))
+
+        try:
+            self.card = notecard.OpenI2C(port, 0, 0, debug=True)
+        except Exception as exception:
+            raise Exception("error opening notecard: "
+                            + NotecardExceptionInfo(exception))
+    
+    else:
+        from periphery import I2C
+        port = I2C("/dev/i2c-1")
+        self.card = notecard.OpenI2C(port, 0, 0)
 
   # Read the temperature from the Notecard’s temperature
   # sensor. The Notecard captures a new temperature sample every
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="notecard-pseudo-sensor",
-    version="1.0.0",
+    version="1.1.0",
     author="Blues Inc.",
     author_email="support@blues.com",
     description="An API interface to the internal sensors of the Blues Wireless Notecard",
